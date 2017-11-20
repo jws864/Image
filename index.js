736c41bee777f67497f7eebfa9fce31176630dcb
Justin Simms <j.simms@mun.ca>
2017-11-20 16:57:45 -0330

    Working Save

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const multer  = require('multer');
 const upload = multer();
 var fs = require('fs')
+var request = require('request');
 
 const port = 8888;
 
@@ -12,26 +13,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const image = [];
 
-app.get('/', (req, res) => {
+app.get('/imagemal/SaveImage', (req, res) => {
     res.json({ image });
 });
 
 app.post('/imagemal/SaveImage', upload.array(), (req, res) => {
     if (req.body && req.body.url) {
-    //not sure how to display inital image
+        console.log(req.body.url)
+        request(req.body.url).pipe(fs.createWriteStream('pic.png'))
+        
         return res.status(200).send();
     }
 
     res.status(400).send();
-})
-
-
-/*
-app.get('/api/imagemal', (req, res) => {
-
-    res.status(200).send(('id: ' + req.query.id));
 });
-*/
+
+app.post('/imagemal/crop', upload.array(), (req, res) => {
+    var w;
+    var h;
+    var x;
+    var y;
+    gm("orginal.png").crop(w,h,x,y)
+});
+
+app.post('/imagemal/resize', upload.array(), (req, res) => {
+    var w;
+    var h;
+    var x;
+    var y;
+    gm("orginal.png").crop(w,h,x,y)
+});
+
+app.post('/imagemal/rotate', upload.array(), (req, res) => {
+    var w;
+    var h;
+    var x;
+    var y;
+    gm("orginal.png").crop(w,h,x,y)
+});
+    
+    
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
